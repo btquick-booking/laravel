abf7ba1b540e50b8244e739dccba02a0476bb1c1
maesamsaberbaedas <maesamsaberbaedas@gmail.com>
2024-04-02 12:04:56 -0700

    chart

--- app/Filament/Widgets/UsersChart.php
+++ app/Filament/Widgets/UsersChart.php
@@ -1,24 +1,19 @@
 <?php
 
-namespace App\Filament\Company\Widgets;
+namespace App\Filament\Widgets;
 
-use Carbon\Carbon;
-use App\Models\Trip;
+use App\Models\User;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
-use PhpParser\Node\Stmt\Return_;
 use Filament\Widgets\ChartWidget;
 
-class TripsChart extends ChartWidget
+class UsersChart extends ChartWidget
 {
-
-
-
-    protected static ?string $heading = 'Chart';
-    protected static string $color = 'success';
+    protected static ?string $heading = 'Blog Users';
+protected static string $color = 'info';
     protected function getData(): array
     {
-        $data = Trend::model(Trip::class)
+        $data = Trend::model(User::class)
         ->between(
             start: now()->startOfMonth(),
             end: now()->endOfMonth(),
@@ -36,11 +31,8 @@ class TripsChart extends ChartWidget
         'labels' => $data->map(fn (TrendValue $value) => $value->date),
     ];
     }
-
-
     protected function getType(): string
     {
         return 'line';
     }
-
 }
